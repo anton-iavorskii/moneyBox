c9c8e6e878bf3bbf2b2af94b375f364b16898374
anton-iavorskii <89117058000@mail.ru>
2021-09-28 16:29:29 +0300

    meta

--- sql/meta.sql
+++ sql/meta.sql
@@ -35,8 +35,6 @@ CREATE DATABASE "moneyboxDB"
     WITH 
     OWNER = moneybox
     ENCODING = 'UTF8'
-    LC_COLLATE = 'ru_RU.UTF-8'
-    LC_CTYPE = 'ru_RU.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
