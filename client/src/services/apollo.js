c084d1d6c48bd7b0c8b8b65aa1a9013587dc435e
anton-iavorskii <89117058000@mail.ru>
2021-09-28 18:12:59 +0300

    add env client

--- client/src/services/apollo.js
+++ client/src/services/apollo.js
@@ -10,7 +10,7 @@ import { onError } from "@apollo/client/link/error";
 
 import { getToken, logout } from "./auth";
 
-const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_SERVER });
 
 const authLink = new ApolloLink(async (operation, forward) => {
   let token = await getToken();
